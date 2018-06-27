4bce82e900f7993a865b28f11b8b86cfb90ade6d
Tony Shindo <honolulu3310@gmail.com>
2018-06-26 21:49:08 -0500

    Add conditional logic to sighup & login components

---
+++ client/components/Dashboard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <div>Your are logged in.</div>
+};
--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -3,6 +3,7 @@ import AuthForm from "./AuthForm";
 import { graphql } from "react-apollo";
 import mutation from "../mutations/Signup";
 import query from "../queries/CurrentUser";
+import { hashHistory } from 'react-router';
 
 class SignupForm extends Component {
   constructor(props) {
@@ -10,6 +11,12 @@ class SignupForm extends Component {
 
     this.state = { errors: [] };
   }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.data.user && !this.props.data.user) {
+      hashHistory.push('/dashboard');
+    }
+  } 
 
   onSubmit({ email, password }) {
     this.props
@@ -36,4 +43,6 @@ class SignupForm extends Component {
   }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+);
